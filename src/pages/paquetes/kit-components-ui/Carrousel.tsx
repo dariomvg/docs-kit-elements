94e74f092b2d93785d2e027e8e81445bda0c11e3
dariomvg <contact.dario.info@gmail.com>
2025-02-14 19:05:03 -0300

    add new component and add tables

--- src/pages/paquetes/kit-components-ui/Carrousel.tsx
+++ src/pages/paquetes/kit-components-ui/Carrousel.tsx
@@ -1,13 +1,10 @@
 import { CarrouselImages } from "kit-components-ui";
-import img1 from "../../../assets/images/image1.jpg";
-import img2 from "../../../assets/images/image2.jpg";
-import img3 from "../../../assets/images/image3.jpg";
+import img1 from "../../../assets/images/image1.webp";
+import img2 from "../../../assets/images/image2.webp";
+import img3 from "../../../assets/images/image3.webp";
 
 export default function Carrousel(): JSX.Element {
   const imgs = [img1, img2, img3];
-  return (
-    <section>
-      <CarrouselImages images={imgs} width="100%" height="500px" />
-    </section>
-  );
+
+  return <CarrouselImages images={imgs} width="640" height="420" />;
 }
---
+++ src/utils/experience.js
@@ -0,0 +1,10 @@
+export const experience = {
+    job_name: "Web development",
+    company: "Your company",
+    first_date: "22 April",
+    last_date: "20 December",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum, magni numquam delectus, autem fugiat ipsa nesciunt optio provident distinctio, dicta enim fugit ducimus? Nobis placeat quia quos labore amet? Animi!",
+    link: "your-link",
+    theme: "",
+  }
